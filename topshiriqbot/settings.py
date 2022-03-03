be75d531d66d69952c392f8df2b17b562c75de9c
MHojimurod <cool.hojimurod2001@mail.ru>
2022-03-03 17:27:31 +0500

    fsdf

--- topshiriqbot/settings.py
+++ topshiriqbot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-osh1ps+8+b3-kvzw_(%9_(wl=d%)!-%aztdj@(e5ok%a^sa-^+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
