432cb05bb864d56f8d9bd48d9f7636e4e2f07fac
MHojimurod <cool.hojimurod2001@mail.ru>
2022-03-04 14:52:50 +0500

    fsdf

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -3,8 +3,7 @@ import os
 import pathlib
 from uuid import uuid4
 import zipfile
-from setuptools import Command
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, PassportElementErrorSelfie, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, Message, PassportElementErrorSelfie, ReplyKeyboardMarkup, ReplyKeyboardRemove, ReplyMarkup, Update
 from telegram.ext import (
     Updater,
     CallbackContext,
@@ -15,9 +14,10 @@ from telegram.ext import (
     CallbackQueryHandler,
     RegexHandler
 )
-from bot.models import Region, User
+from bot.models import Region, Task, User
 from excel import makeexcelData
 from utils import distribute
+
 
 
 root = pathlib.Path(__file__).resolve().parent.parent
@@ -32,9 +32,11 @@ tasks_number = 10
 
 
 
+
 class Bot(Updater):
     def __init__(self, *args, **kwargs):
-        super(Bot, self).__init__("5263596793:AAGp-Mwn4tw0v1u0TsxbhtInPmt-yDYzvBI")
+        super(Bot, self).__init__(
+            "5263596793: AAGp-Mwn4tw0v1u0TsxbhtInPmt-yDYzvBI")
 
         not_start = ~Filters.regex("^/start$")
         self.conversation = ConversationHandler(
@@ -76,14 +78,83 @@ class Bot(Updater):
             ]
         )
         self.dispatcher.add_handler(CommandHandler('data', self.data))
+        self.dispatcher.add_handler(CallbackQueryHandler(self.accept_task, pattern="^accept_answer"))
+        self.dispatcher.add_handler(CallbackQueryHandler(self.reject_task, pattern="^reject_answer"))
         self.dispatcher.add_handler(self.conversation)
         self.start_polling()
         self.idle()
+    
+    def accept_task(self, update:Update, context:CallbackContext):
+        # user:User = User.objects.filter(chat_id=update.callback_query.from_user.id).first()
+        # if user.panding_answer():
+        #     update.callback_query.answer("Sizda yuborilgan javobingizni kiriting!")
+        #     return TASKS
+        # else:
+        #     update.callback_query.answer("Sizda yuborilgan javobingizni kiriting!")
+        #     return TASKS
+        data = update.callback_query.data.split(":")
+        task:Task = Task.objects.filter(id=int(data[1])).first()
+        if task:
+            task.accept()
+            update.callback_query.answer("Javobingiz qabul qilindi!")
+            for message in task.messages():
+                context.bot.edit_message_caption(message.chat_id, message.message_id, caption="Bu javob tasdiqlandi!")
+            
+            context.bot.send_message(task.user.chat_id,
+                "Sizning javobingiz qabul qilindi!\nKeyingi topshiriqqa o'tishingiz mumkin!")
+            c = task.user.curent_task()
+            if c:
+                context.bot.send_message(task.user.chat_id, c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", parse_mode="HTML")
+                return TASKS
+            else:
+                context.bot.send_message(
+                    task.user.chat_id, "Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz")
+                zipFile = zipfile.ZipFile(f"{task.user.chat_id}_{task.user.name}.zip", 'w')
+                tasks = task.user.tasks()
+                for task in tasks:
+                    print(f"{root}")
+                    zipFile.write(os.path.join(root, task.document), os.path.relpath(os.path.join(root, task.document)))
+                    os.remove(task.document)
+                zipFile.close()
+                for admin in admins:
+                    context.bot.send_document(chat_id=admin,document=open(f"{task.user.chat_id}_{task.user.name}.zip", 'rb'))
+            return TASKS
+    
+    def reject_task(self, update:Update, context:CallbackContext):
+        data = update.callback_query.data.split(":")
+        task:Task = Task.objects.filter(id=int(data[1])).first()
+        if task:
+            task.reject()
+            update.callback_query.answer("Javobingiz qabul qilindi!")
+            for message in task.messages():
+                context.bot.edit_message_caption(
+                    message.chat_id, message.message_id, caption="Bu javob rad etib bo'lindi!!")
+            c = task.user.curent_task()
+            context.bot.send_message(task.user.chat_id,
+                "Sizning javobingiz rad etildi!\nIstasangiz qayta yuboring yoki bo'ttan yoqolin!")
+            if c:
+                context.bot.send_message(task.user.chat_id, c.description +
+                                         "\n<b>.docx .pdf text</b> formatida yuboring!", parse_mode="HTML")
+                return TASKS
+            else:
+                context.bot.send_message(
+                    task.user.chat_id, "Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz")
+                zipFile = zipfile.ZipFile(f"{task.user.chat_id}_{task.user.name}.zip", 'w')
+                tasks = task.user.tasks()
+                for task in tasks:
+                    print(f"{root}")
+                    zipFile.write(os.path.join(root, task.document), os.path.relpath(os.path.join(root, task.document)))
+                    os.remove(task.document)
+                zipFile.close()
+                for admin in admins:
+                    context.bot.send_document(chat_id=admin,document=open(f"{task.user.chat_id}_{task.user.name}.zip", 'rb'))
+            return TASKS
     
     def answer_sent_user(self, update:Update, context:CallbackContext):
         user:User = User.objects.filter(chat_id=update.message.from_user.id).first()
         if update.message.text:
             context.user_data['current_data'] = update.message.text
+            context.user_data['current_data_type'] = 0
             update.message.reply_text("To'liq javobingiz shumi yoki qayta yuboarsizmi!", reply_markup=ReplyKeyboardMarkup(
                 [
                     [
@@ -93,6 +164,7 @@ class Bot(Updater):
             , resize_keyboard=True))
         
         elif update.message.document:
+            context.user_data['current_data_type'] = 1
             context.user_data['current_data'] = update.message.document.get_file().download(f"files/{user.curent_task().id}_{str(uuid4())}_{update.message.document.file_name}")
             update.message.reply_text("To'liq javobingiz shumi yoki qayta yuboarsizmi!", reply_markup=ReplyKeyboardMarkup(
                 [
@@ -106,57 +178,66 @@ class Bot(Updater):
         user:User = User.objects.filter(chat_id=update.message.from_user.id).first()
         if update.message.text.lower() == "ha":
             data = context.user_data['current_data']
-            if isinstance(data, str):
-                if user:
-                    if user.tasks().count() < tasks_number:
+            
+            if user:
+                if user.tasks().count() < tasks_number:
+                    if context.user_data['current_data_type'] == 0:
                         filename = f"files/{user.curent_task().id}_{str(uuid4())}_answer.txt"
                         file = open(filename, 'w')
                         file.write(data)
                         file.close()
-                        user.add_task(filename)
-                        c = user.curent_task()
-                        if c:
-                            update.message.reply_html(c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
-                            return TASKS
-                        else:
-                            update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz")
-                            zipFile = zipfile.ZipFile(f"{user.chat_id}_{user.name}.zip", 'w')
-                            tasks = user.tasks()
-                            for task in tasks:
-                                print(f"{root}")
-                                zipFile.write(os.path.join(root, task.document), os.path.relpath(os.path.join(root, task.document)))
-                                os.remove(task.document)
-                            zipFile.close()
-                            for admin in admins:
-                                context.bot.send_document(chat_id=admin,document=open(f"{user.chat_id}_{user.name}.zip", 'rb'))
+                    else:
+                        filename = context.user_data['current_data']
+                    answer: Task = user.add_task(filename, context.user_data['current_data_type'])
+                    for admin in admins:
+                        try:
+                            # message:Message = context.bot.send_message(admin, "gdfgdfg", reply_markup=InlineKeyboardMarkup(
+                            #     [
+                            #         [
+                            #             InlineKeyboardButton( "Qabul qilish", callback_data=f"accept_answer:{answer.id}"),
+                            #             InlineKeyboardButton( "Rad etish", callback_data=f"reject_answer:{answer.id}")
+                            #         ]
+                            #     ]
+                            # ))
+                            message:Message = context.bot.send_document(admin, document=open(filename, 'rb'), reply_markup=InlineKeyboardMarkup(
+                                [
+                                    [
+                                        InlineKeyboardButton( "Qabul qilish", callback_data=f"accept_answer:{answer.id}"),
+                                        InlineKeyboardButton( "Rad etish", callback_data=f"reject_answer:{answer.id}")
+                                    ]
+                                ]
+                            ))
+                            answer.send(message)
+
+                        except Exception as e:
+                            print(e)
+                    update.message.reply_text("Javobingiz krisalarga yuborildi!\nTez orada  yo yaxshi bo'ladi yo' yaxshilikka bo'ladi!\n\nUngacha siqilib turin!", reply_markup=ReplyKeyboardRemove())
+                    # c = user.curent_task()
+                    # if c:
+                    #     update.message.reply_html(c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
+                    #     return TASKS
+                    # else:
+                    #     update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz")
+                    #     zipFile = zipfile.ZipFile(f"{user.chat_id}_{user.name}.zip", 'w')
+                    #     tasks = user.tasks()
+                    #     for task in tasks:
+                    #         print(f"{root}")
+                    #         zipFile.write(os.path.join(root, task.document), os.path.relpath(os.path.join(root, task.document)))
+                    #         os.remove(task.document)
+                    #     zipFile.close()
+                    #     for admin in admins:
+                    #         context.bot.send_document(chat_id=admin,document=open(f"{user.chat_id}_{user.name}.zip", 'rb'))
+                else:   
+                    update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz")
+                
 
 
-                    else:   
-                        update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz")
-            else:
-                if user:
-                    if user.tasks().count() < tasks_number:
-                        file = data
-                        user.add_task(file)
-                        c = user.curent_task()
-                        if c:
-                            update.message.reply_html(c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
-                            return TASKS
-                        else:
-                            update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz", reply_markup=ReplyKeyboardRemove())
-                            zipFile = zipfile.ZipFile(f"{user.chat_id}_{user.name}.zip", 'w')
-                            tasks = user.tasks()
-                            for task in tasks:
-                                zipFile.write(os.path.join(root, task.document), os.path.relpath(os.path.join(root, task.document)))
-                                os.remove(task.document)
-                            zipFile.close()
-                            for admin in admins:
-                                context.bot.send_document(chat_id=admin,document=open(f"{user.chat_id}_{user.name}.zip", 'rb'))
-                    else:   
-                        update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz", reply_markup=ReplyKeyboardRemove())
+
         elif update.message.text.lower() == "qayta yuborish":
-            update.message.reply_html( user.curent_task().description + "<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
+            update.message.reply_html(user.curent_task(
+            ).description + "<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
             return TASKS
+    
 
     def start(self, update:Update, context:CallbackContext):
         """start a new conversation"""
@@ -166,11 +247,15 @@ class Bot(Updater):
             update.message.reply_text("Assalom alekum Xush kelibsiz Ism Familyangizni kiriting!",reply_markup=ReplyKeyboardRemove())
             return NAME
         else:
+            if user.panding_answer():
+                update.message.reply_text("Iltimos yuborgan javobingizni javobi kelishini kuting!")
+                return TASKS
             c = user.curent_task()
             if c:
 
                 update.message.reply_text("Topshiriqlarni yuboring!")
-                update.message.reply_html(c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
+                update.message.reply_html(c.description + "\n<b>.docx .pdf text</b> formatida yuboring!",
+                                          reply_markup=ReplyKeyboardRemove())
                 return TASKS
             else:
                 update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz")
@@ -211,7 +296,8 @@ class Bot(Updater):
                 update.message.reply_text("Muvaffaqiyatli ro'yxatdan o'tdingiz!\nIltimos endi topshiriqlarni yuboring!")
                 c = user.curent_task()
                 if c:
-                    update.message.reply_html(c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
+                    update.message.reply_html(
+                        c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
                     return TASKS
                 else:
                     update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz")
@@ -232,7 +318,8 @@ class Bot(Updater):
                 user.add_task(file)
                 c = user.curent_task()
                 if c:
-                    update.message.reply_html(c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
+                    update.message.reply_html(
+                        c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
                     return TASKS
                 else:
                     update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz", reply_markup=ReplyKeyboardRemove())
@@ -250,7 +337,8 @@ class Bot(Updater):
                 update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz")
     
     def error_type(self, update:Update, context:CallbackContext):
-        update.message.reply_html("Kechirasiz javoblaringizni faqat <b>.docx .pdf text</b> formatida yubora olasiz!", reply_markup=ReplyKeyboardRemove())
+        update.message.reply_html("Kechirasiz javoblaringizni faqat <b>.docx .pdf text</b> formatida yubora olasiz!",
+                                  reply_markup=ReplyKeyboardRemove())
 
 
     def task_text(self, update:Update, context:CallbackContext):
@@ -264,7 +352,8 @@ class Bot(Updater):
                 user.add_task(filename)
                 c = user.curent_task()
                 if c:
-                    update.message.reply_html(c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
+                    update.message.reply_html(
+                        c.description + "\n<b>.docx .pdf text</b> formatida yuboring!", reply_markup=ReplyKeyboardRemove())
                     return TASKS
                 else:
                     update.message.reply_text("Siz topshiriqlarni yakunladingiz ishtirokingiz uchun raxmat. Biz sizga tez orada aloqaga chiqamiz", reply_markup=ReplyKeyboardRemove())
